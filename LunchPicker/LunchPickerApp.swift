4c18c5f8eca8c62c9ff5cd1dcfba3359bfe77edf
Tommer <souirtommer@gmail.com>
2023-05-10 19:08:43 +0800

    Initial Commit

---
+++ LunchPicker/LunchPickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LunchPickerApp.swift
+//  LunchPicker
+//
+//  Created by ST SE on 10/5/2023.
+//
+
+import SwiftUI
+
+@main
+struct LunchPickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
